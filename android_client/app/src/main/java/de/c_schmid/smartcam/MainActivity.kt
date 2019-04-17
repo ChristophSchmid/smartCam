884e33d00048c85541c117d5927bbb3c349624af
c-schmid <c-schmid@gmx.net>
2019-04-17 21:34:09 +0200

    If applied, this commit will improve the async Task implementation
    The async task implementation was enhanced. All code for socket communication
    was moved into the async task. Memory leaking was prevented by use of
    WeakReference.

--- android_client/app/src/main/java/de/c_schmid/smartcam/MainActivity.kt
+++ android_client/app/src/main/java/de/c_schmid/smartcam/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.ProgressDialog
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.Toast
 import com.squareup.picasso.Picasso
@@ -12,44 +13,36 @@ import timber.log.Timber
 import java.io.File
 import java.io.IOException
 import java.lang.Exception
+import java.lang.ref.WeakReference
 import java.net.InetAddress
 import java.net.Socket
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var TMPFILE: File
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        TMPFILE = File.createTempFile("IMG", ".png", applicationContext.cacheDir)
 
         button_take_picture.setOnClickListener {
-
-            Timber.v("Trying to receive image from remote camera")
-            val tmpimg = GetImage(false).execute(false).get()
-            Timber.d("Received image from camera to file: $tmpimg")
-            if(tmpimg != "") {
-                Timber.d("Loading image into camera_image view")
-                Picasso.get().load(TMPFILE).into(camera_image)
-            }
-            else {
-                Toast.makeText(applicationContext,"Camera not available",Toast.LENGTH_SHORT).show()
-            }
+            GetImage(false, WeakReference(camera_image)).execute()
         }
     }
 
-    inner class GetImage(private val useFlash: Boolean = false): AsyncTask<Boolean, Int, String>() {
+    class GetImage(private val useFlash: Boolean = false,
+                   private val imgView: WeakReference<ImageView>
+                   ): AsyncTask<Unit, Int, Unit>() {
 
         private val MESSAGE_SEND = "SEND_IMG"
+        private val TMPFILE = File.createTempFile("IMG", ".png", imgView.get()?.context?.cacheDir)
 
         var exception: Exception? = null
 
         override fun onPreExecute() {
             super.onPreExecute()
+            Timber.v("Trying to receive image from remote camera")
         }
 
-        override fun doInBackground(vararg params: Boolean?): String {
+        override fun doInBackground(vararg params: Unit) {
             val socket = Socket(InetAddress.getByName("192.168.178.27"), 12345)
             val outputToCamera = socket.getOutputStream().bufferedWriter()
             val inputFromCamera = socket.getInputStream()
@@ -66,14 +59,24 @@ class MainActivity : AppCompatActivity() {
             }
             catch(e: IOException) {
                 this.exception = e
-                return ""
             }
             finally {
                 socket.close()
             }
+        }
 
-            Timber.d("Successfully received image")
-            return TMPFILE.absolutePath
+        override fun onPostExecute(result: Unit?) {
+            super.onPostExecute(result)
+
+            Timber.d("Received image from camera to file: $TMPFILE")
+
+            if(exception == null) {
+                Timber.v("Loading image into camera_image view")
+                Picasso.get().load(TMPFILE).into(imgView.get())
+            }
+            else {
+                Toast.makeText(imgView.get()?.context,"Camera not available",Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
